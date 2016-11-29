f8b9f775e672687f3781bb4ceba246f32e931dd9
dev4anderson <nds.anderson@gmail.com>
2016-11-29 20:54:20 -0200

    atualização e novos recursos

--- app/src/main/java/com/example/andersonmacedo/timesfutebol/DetalheActivity.java
+++ app/src/main/java/com/example/andersonmacedo/timesfutebol/DetalheActivity.java
@@ -1,12 +1,22 @@
 package com.example.andersonmacedo.timesfutebol;
 
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.andersonmacedo.timesfutebol.model.Time;
+import com.squareup.picasso.Picasso;
+
 
 public class DetalheActivity extends AppCompatActivity {
+
+    TextView  tvNomeDet;
+    TextView  tvEstadoDet;
+    TextView  tvAnoFundacaoDet;
+    ImageView ivEscudoDet;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +25,24 @@ public class DetalheActivity extends AppCompatActivity {
 
         if (getIntent() != null){
             Time time = getIntent().getParcelableExtra("Time");
-            Toast.makeText(this, time.getNome(),Toast.LENGTH_SHORT).show();
+
+            tvNomeDet = (TextView) findViewById(R.id.tvNomeDet);
+            tvEstadoDet = (TextView) findViewById(R.id.tvEstadoDet);
+            tvAnoFundacaoDet = (TextView) findViewById(R.id.tvAnoFundacaoDet);
+            ivEscudoDet = (ImageView) findViewById(R.id.ivEscudoDet);
+
+            tvNomeDet.setText(time.getNome().toString());
+            tvEstadoDet.setText(time.getEstado().toString());
+            tvAnoFundacaoDet.setText(time.getAnoFundacao().toString());
+
+            Picasso.with(this)
+                    .load(time.getEscudo().toString())
+                    .into(ivEscudoDet);
+
         }
 
     }
+
+
+
 }
